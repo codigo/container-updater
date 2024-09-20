2de38f07af9d1afa3f2a0991e37956f0c65bd395
Mauricio Mercado <maumercado@gmail.com>
2024-09-19 21:13:14 -0700

    feat: enhance server initialization and logging (#7)
    * feat: enhance server initialization and logging
    
    Add a new server initialization section to `src/app.ts` that
    creates a Fastify server instance and listens on port 3000. 
    Replace the logger customization with a logger setup in the 
    server instantiation. Update the command in the Dockerfile to 
    use an array format. Update `.nvmrc` to specify Node.js version 
    22 for consistency.
    
    * fix: correct service name in test workflow file
    
    Update the name of the test workflow from "Test Servic" to 
    "Test Service" to reflect the correct spelling and ensure 
    consistency across project documentation.
    
    * fix(eslint): add global settings for TypeScript files
    
    Add global settings for Node.js and ES2021 to the ESLint configuration 
    for TypeScript files. This improves linting accuracy by recognizing 
    global variables in the specified environments, ensuring better 
    integration with the project’s codebase.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,17 @@
 import typescript from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
   {
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2021
+      },
       parser: tsParser,
     },
     plugins: {
